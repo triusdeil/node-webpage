d0cb79bf27522bbb3a8fb83a014aa745c2c1d6d5
triusdeil <triusdeil@gmail.com>
2020-11-26 14:10:23 -0400

    routes, index.ejs, main.js

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -1,11 +1,10 @@
 const {Router} = require('express');
 const router = Router();
 const path = require('path')
+const ejs = require('ejs')
 
 router.get('/', (req,res) =>{
     res.render('index');
 })
-
-
 
 module.exports = router;
